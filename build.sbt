5f2da304cc5ce1b776fad8521473483cdd3cb981
FhizZx <stefanmanol@gmail.com>
2020-05-21 22:55:25 +0100

    first commit

---
+++ build.sbt
@@ -0,0 +1,48 @@
+import Dependencies._
+
+ThisBuild / scalaVersion     := "2.12.8"
+ThisBuild / version          := "0.1.0-SNAPSHOT"
+ThisBuild / organization     := "com.example"
+ThisBuild / organizationName := "example"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "Meandering Depths",
+	libraryDependencies  ++= Seq(
+		// Last stable release
+		"org.scalanlp" %% "breeze" % "1.0",
+
+		// Native libraries are not included by default. add this if you want them
+		// Native libraries greatly improve performance, but increase jar sizes.
+		// It also packages various blas implementations, which have licenses that may or may not
+		// be compatible with the Apache License. No GPL code, as best I know.
+		"org.scalanlp" %% "breeze-natives" % "1.0",
+
+		// The visualization library is distributed separately as well.
+		// It depends on LGPL code
+		"org.scalanlp" %% "breeze-viz" % "1.0",
+
+		"org.lwjgl" % "lwjgl" % "3.2.2",
+
+		"org.lwjgl" % "lwjgl" % "3.2.2" classifier s"natives-windows",
+
+		"org.lwjgl" % "lwjgl-opengl" % "3.2.2",
+
+		"org.lwjgl" % "lwjgl-opengl" % "3.2.2" classifier s"natives-windows",
+
+		"org.lwjgl" % "lwjgl-glfw" % "3.2.2",
+
+		"org.lwjgl" % "lwjgl-glfw" % "3.2.2" classifier s"natives-windows",
+
+    "org.scalactic" %% "scalactic" % "3.1.2",
+
+    "org.scalatest" %% "scalatest" % "3.1.2" % "test",
+
+    "org.scalacheck" %% "scalacheck" % "1.14.1" % "test",
+	)
+
+
+
+  )
+
+// See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
---
+++ src/main/scala/maths/GraphicsMaths.scala
@@ -0,0 +1,64 @@
+package maths
+import breeze.linalg._
+import scala.math._
+
+object GraphicsMaths {
+
+  //State: Vector3 is a 3 dimensional vector of floats
+  type Vector3 = DenseVector[Float]
+  def Vector3(): Vector3 = DenseVector.zeros[Float](3)
+  def Vector3(a: Float, b: Float, c: Float): Vector3 = DenseVector[Float](a,b,c)
+
+  //State: Vector4 is a 4 dimensional vector of floats
+  type Vector4 = DenseVector[Float]
+  def Vector4(): Vector4 = DenseVector.zeros[Float](3)
+  def Vector4(a: Float, b: Float, c: Float, d: Float): Vector4 = DenseVector[Float](a,b,c,d)
+  def Vector4(v: Vector3, w: Float): Vector4 = DenseVector[Float](v(0),v(1),v(2),w)
+
+  //State: Matrix4 is a 4x4 matrix of floats
+  type Matrix4 = DenseMatrix[Float]
+  def Matrix4(): Matrix4 = DenseMatrix.eye[Float](4)
+
+  //expands 3x3 matrix to 4x4 matrix
+  def expand(a: DenseMatrix[Float]): Matrix4 = {
+    require(a.rows == 3 && a.cols == 3)
+    DenseMatrix.horzcat(DenseMatrix.vertcat(a,DenseMatrix.zeros[Float](1,3)),DenseMatrix((0F),(0F),(0F),(1F)))
+  }
+
+  //creates a rotation matrix corresponding to a rotation of angle around axis
+  //TODO: compute the matrix using quaternions to reduce precision loss
+  def rotate(axis: Vector3, angle: Double): Matrix4 = {
+    val u = normalize(axis)
+    val c = cos(angle%(2*Pi)).toFloat
+    val s = sin(angle%(2*Pi)).toFloat
+    var R = c * DenseMatrix.eye[Float](3) + (1 - c) * u * u.t
+    R(0,1) -= s * u(2); R(0,2) += s * u(1); R(1,0) += s * u(2); R(1,2) -= s * u(0); R(2,0) -= s * u(1); R(2,1) += s * u(0)
+    expand(R)
+  }
+
+  //creates a translation matrix corresponding to a translation by displacement
+  def translate(displacement: Vector3): Matrix4 = {
+    var m = DenseMatrix.eye[Float](4)
+    m(0,3) = displacement(0); m(1,3) = displacement(1); m(2,3) = displacement(2)
+    m
+  }
+
+  //creates a scaling matrix
+  def scale(x: Float, y: Float, z: Float): Matrix4 = diag(DenseVector(x,y,z,1F))
+
+  //creates a perspective projection matrix given the value of the vertical
+  // field of view (in radians), the aspect ratio (width/height) and how
+  // close the near and far clipping planes are
+  // far - near should be as small as possible to avoid z-fighting
+  def perspectiveProjection
+  (FoV: Double, aspectRatio: Float, nearPlane: Float, farPlane: Float): Matrix4 = {
+    var m =  DenseMatrix.zeros[Float](4,4)
+    val ct = (1 / tan(FoV/2)).toFloat
+    m(1,1) = ct
+    m(0,0) = ct / aspectRatio
+    m(2,2) = -(farPlane + nearPlane)/(farPlane - nearPlane)
+    m(2,3) = -2 * farPlane * nearPlane/(farPlane - nearPlane)
+    m(3,2) = -1
+    m
+  }
+}
