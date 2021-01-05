61e870fffc892a6e0a59e19c08fc9e000dabd10e
FhizZx <35220233+FhizZx@users.noreply.github.com>
2021-01-05 14:26:37 +0200

    Update FixedTimeStepLoop.scala
    Added more comments

--- src/main/scala/core/FixedTimeStepLoop.scala
+++ src/main/scala/core/FixedTimeStepLoop.scala
@@ -3,16 +3,29 @@ package core
 import library.GLFW
 import library._
 
+/** Generic game loop that decouples the framerate of the game from the actual time simulated inside the game, so that the performance of the computer does not affect the game
+    logic. It also ensures decoupling between the input handling, game logic and rendering, in MVC style. */
+
 trait FixedTimeStepLoop {
+    
+    /** Desired number of ticks (updates of game logic) per second. This value should probably be set by the class implementing this trait, but this works for now. */
     val TARGET_TPS = 30
+    
     private var isRunning = false
 
+    /** GLFW is needed here for the timer, but there is no reason not to use the system's timer directly from some standard java/scala library, in order to avoid this
+        dependence. To make this trait truly generic, this should be changed later. */
     GLFW.init()
 
+    
+    /** The actual fixed time loop, running the game. */
     final def loop() = {
+        
         val loopTimer = new Timer()
         loopTimer.start()
+        
         isRunning = true
+        
         //Keeps track of the time not yet simulated
         var accumulator = 0D
 
@@ -34,12 +47,15 @@ trait FixedTimeStepLoop {
                 accumulator -= tick
             }
 
-            /**Render an interpolated image between the previous and current game state
-            Note: Ideally, the interpolation would be done between the current state and the next one, but this requires the extrapolation of that future state. By using the previous state, the extrapolation is not needed, but there will be some very slight latency (bounded by the tick duration). */
+            /** Render an interpolated image between the previous and current game state
+                Note: Ideally, the interpolation would be done between the current state and the next one, but this requires the extrapolation of that future state. By using the
+                previous state, the extrapolation is not needed, but there will be some very slight latency (bounded by the tick duration). */
             val alpha = accumulator / tick
             render(alpha)
         }
     }
+    
+    /** What the game actually does is decided by these 3 functions, which need to be implemented. */
     protected def input(): Unit
 
     protected def update(dt: Double): Unit
